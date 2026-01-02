beeb369b9e12861f269b2b8cbc195cb7e4da7086
RubenLWF <ruben.lagerwerf@gmail.com>
2026-01-02 15:27:58 +0100

    fix: Set CORS header to correct domain

--- api/token.ts
+++ api/token.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://rubenlwf.github.io');
+  res.setHeader('Access-Control-Allow-Origin', 'https://rubenlagerwerf.nl');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
